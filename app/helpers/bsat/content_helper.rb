671e0297dc0a2526d188ddd0d4f14f45b21fa181
René Sprotte <rene.sprotte@provideal.net>
2014-02-19 13:34:17 +0100

    Helper for brand title.

---
+++ app/helpers/bsat/content_helper.rb
@@ -0,0 +1,26 @@
+module Bsat
+  module ContentHelper
+
+    def bsat_brand(title, path: nil)
+      content_for(:bsat_brand, title)
+      content_for(:bsat_brand_path, path) if path.present?
+    end
+
+    def bsat_sidebar_nav
+      sidebar_nav = SidebarNav.new(self)
+      yield(sidebar_nav) if block_given?
+      content_for(:bsat_sidebar_nav_items, sidebar_nav.render)
+    end
+
+    def bsat_page_title(title)
+      content_for(:bsat_page_title, title)
+    end
+
+    def bsat_page_actions
+      page_actions = PageActions.new(self)
+      yield(page_actions) if block_given?
+      content_for(:bsat_page_actions, page_actions.render)
+    end
+
+  end
+end
