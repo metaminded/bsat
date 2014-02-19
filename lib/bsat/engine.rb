f1838a035d10def0642e8a367b5bb52e6094335a
René Sprotte <rene.sprotte@provideal.net>
2014-02-19 10:21:36 +0100

    Make helper available and allow auto-load in dev mode.

--- lib/bsat/engine.rb
+++ lib/bsat/engine.rb
@@ -5,5 +5,12 @@ require 'compass-rails'
 module Bsat
   class Engine < ::Rails::Engine
     engine_name 'bsat'
+
+    # Make helpers available to host app
+    config.to_prepare do
+      ActiveSupport.on_load(:action_controller) do
+        helper Bsat::Engine.helpers
+      end
+    end
   end
 end
